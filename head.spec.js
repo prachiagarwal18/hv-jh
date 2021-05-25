0a9cccd4ea5223a17861e8e1454176bd398ff372
Prachi <prachagarwal18.pa@gmail.com>
2021-05-25 16:52:07 +0530

    third commit

--- head.spec.js
+++ head.spec.js
@@ -4,5 +4,8 @@ describe('Head',() => {
         var arr=[]
         expect(head(arr)).toEqual(undefined);
     });
-    
+    it('Should return first element of array',()=>{
+        var arr=[2,3,4,5]
+        expect(head(arr)).toEqual(2);
+      });
 })
